827aa6c1a7dcbb0ecdd3ed84ab894cda44eef02a
abythomas300 <abythomas300@gmail.com>
2025-09-22 00:39:23 +0530

    feat: add template and image

---
+++ react-facts/src/App.jsx
@@ -0,0 +1,12 @@
+import reactLogo from "./assets/react-logo-dark.png"
+
+function App() {
+  return(
+    <div>
+      <h2>React Facts!</h2>
+      <img src={reactLogo} alt="react-logo-dark" width={500} height={500}/>
+    </div>
+  )
+}
+
+export default App

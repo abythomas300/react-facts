0d9376279a3818d6751c49c78b7bfbf2641adee3
abythomas300 <abythomas300@gmail.com>
2025-09-22 01:14:22 +0530

    style : add basic style

--- react-facts/src/App.jsx
+++ react-facts/src/App.jsx
@@ -1,10 +1,21 @@
 import reactLogo from "./assets/react-logo-dark.png"
+import "./app.css"
 
 function App() {
   return(
-    <div>
-      <h2>React Facts!</h2>
-      <img src={reactLogo} alt="react-logo-dark" width={500} height={500}/>
+    <div className="main-container">
+      <h2>Why</h2>
+      <img src={reactLogo} alt="react-logo-dark" className="logo"/>
+      <h2>is cool?</h2>
+      <div className="main-container">
+        <ul>
+          <li>Virtual DOM</li>
+          <li>Components make UI reusable and easy to code</li>
+          <li>Better documentation</li>
+          <li>Companies love it</li>
+          <li>Always improving</li>
+        </ul>
+      </div>
     </div>
   )
 }

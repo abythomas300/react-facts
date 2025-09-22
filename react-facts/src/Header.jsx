10e190e36b82985701c62fffea6b864c2f02cb17
abythomas300 <abythomas300@gmail.com>
2025-09-22 23:36:36 +0530

    feat: add navbar and basic page style

--- react-facts/src/Header.jsx
+++ react-facts/src/Header.jsx
@@ -3,11 +3,23 @@ import "./App.css"
 
 function Header() {
     return(
-        <header>
+        <>
+            <header>
             <h2>Why</h2>
             <img src={reactLogo} alt="react-logo-dark" className="logo"/>
             <h2>is cool?</h2>
-        </header>
+            </header>
+
+            <nav>
+                <ul>
+                    <li><a href="https://react.dev/learn">Learn</a></li>
+                    <li><a href="https://react.dev/reference/react">Reference</a></li>
+                    <li><a href="https://react.dev/community">Community</a></li>
+                    <li><a href="https://react.dev/blog">Blog</a></li>
+                </ul>
+            </nav>
+
+        </>
     )
 }
 
